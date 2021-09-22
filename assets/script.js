29c84a5fad2d6294fcd58e8d589f9ae9ff7d6680
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2021-09-21 21:15:32 -0400

    Added solution

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ0123456789+-!?_$@&';
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(writePassword) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  // for (var i = 10; i < passwordLenght; i++);{
+  // var random = getRandomInt(characters.length);
+  // generatePassword += characters[random];
+
+}
+
+function generatePassword(){
+  var password = '';
+  for (var i = 0; i < 8; i++){
+    var randomNumber = getRandomInt(characters.length)
+    var addChar = characters[randomNumber];
+    password = password + addChar;
+  }
+  return password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getRandomInt(max) {
+  var random = Math.random();
+  random *= max;
+  return Math.floor(random);
+}
